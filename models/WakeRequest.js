02f156d4355d3a53268d110037468fa0af74b9b5
Ryan Haack <ryan@haackr.com>
2021-08-17 08:26:53 -0600

    Add WakeRequest model

---
+++ models/WakeRequest.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const wakeRequestSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+  },
+  computer: {
+    type: mongoose.Types.ObjectId,
+    ref: "Computer",
+  },
+  status: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.Model("WakeRequest", wakeRequestSchema);
