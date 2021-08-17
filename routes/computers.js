f2dce8c36f80c02b0482c8d26971c20286d68ca7
Ryan Haack <ryan@haackr.com>
2021-08-17 10:19:15 -0600

    Add some routes for computers

--- routes/computers.js
+++ routes/computers.js
@@ -9,4 +9,50 @@ router.get("/", async (req, res) => {
   res.send(computers);
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const computer = await Computer.findById(req.params.id);
+    res.send(computer);
+  } catch (error) {
+    console.log(error);
+    res.status(404).send();
+  }
+});
+
+router.post("/", async (req, res) => {
+  if (!req.body.computer)
+    res.status(400).send("You must send a computer object in the body!");
+
+  console.log(req.body.computer);
+  try {
+    const newComputer = await Computer.create(req.body.computer);
+    res.send({ computer: newComputer });
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await Computer.deleteOne({ _id: req.params.id });
+    res.status(200).send();
+  } catch (error) {
+    console.log(error);
+    res.status(404).send();
+  }
+});
+
+router.patch("/:id", async (req, res) => {
+  try {
+    const updatedComputer = await Computer.updateOne(
+      { _id: req.params.id },
+      { ...req.body.computer }
+    );
+    res.status(200).send();
+  } catch (error) {
+    console.log(error);
+    res.status(404).send();
+  }
+});
+
 module.exports = router;
